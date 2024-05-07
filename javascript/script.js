949a8ab8ecf7cb993bd3e294cb63ad9812830f54
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-05-07 14:41:22 +0100

    function

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,47 @@
 // const changeButton = document.querySelector(".clock")
 
-document.getElementById("clock").style.color ="blue";
+// document.getElementById("clock").style.color ="blue";
+
+//  to set a digital clock
+
+let hrs = document.getElementById("hrs");
+let mins = document.getElementById("mins");
+let secs = document.getElementById("secs");
+let pm = document.getElementById('am')
+
+setInterval(() => {
+  let currentTime = new Date()
+
+  hrs.innerHTML = currentTime.toLocaleTimeString("en-NG", {
+    hour12: true
+  }).split(":")[0]
+
+  mins.innerHTML =
+  currentTime.getMinutes() < 10
+  ? `0${currentTime.getMinutes()}`
+  : currentTime.getMinutes();
+
+  secs.innerHTML =
+    currentTime.getSeconds() < 10
+      ? `0${currentTime.getSeconds()}`
+      : currentTime.getSeconds();
+
+  // hrs.innerHTML= currentTime.getHours()<10 ? "0": "" + currentTime.getHours();
+  // mins.innerHTML = currentTime.getMinutes()<10?"0": "" +  currentTime.getMinutes();
+  // secs.innerHTML = currentTime.getSeconds()<10?"0": "" +  currentTime.getSeconds();
+
+  pmgit.innerHTML = currentTime.toLocaleTimeString("en-NG", {
+    hour12 : true
+}).split(" ")[3]
+
+}, 1000);
+
+
+
+console.log("currentTime");
+
+//  const hr = new Date();
+
+//  document.getElementById("hrs").innerHTML = hr;
+
+//  console.log("hr");
